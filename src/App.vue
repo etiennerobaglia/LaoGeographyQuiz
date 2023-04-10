5f9db1a97764742d2785eefe80795b2c56246321
etienne robaglia <etienne.robaglia@ext.mpsa.com>
2023-04-10 16:03:36 +0700

    inital commit: project setup vite + mapbox

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="map" />
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { onMounted } from "vue";
+export default {
+  setup() {
+    onMounted(() => {
+      mapboxgl.accessToken =
+        import.meta.env.VITE_VUE_APP_MAPBOX_TOKEN;
+      const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v12",
+      });
+      map.on('load', () => {
+      // TODO: Here we want to load a layer
+      // TODO: Here we want to load/setup the popup
+      });
+    });
+    return {};
+  },
+};
+</script>
+
+<style>
+#map {
+  width: 50%;
+  height: 100%;
+}
+</style>
